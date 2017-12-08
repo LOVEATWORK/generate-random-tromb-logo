b285eb70aea09ebe046d7c51978ca5d44e855699
Lars Huring <lars.huring@gmail.com>
2017-12-08 14:53:17 +0100

    Initial commit
    Generate random Tromb logo and save it as SVG

---
+++ js/generate.js
@@ -0,0 +1,60 @@
+var colors = [
+    '#FFFFFF',
+    '#000000',
+    '#428670',
+    '#48141A',
+    '#E3993F',
+    '#CA393B',
+    '#12474C'
+];
+
+$("document").ready(function() {
+    var color = getRandomItem()
+    var background = getRandomItem();
+    
+    if (color == background)
+        background = getRandomItem();
+    
+    console.log(color + ' & ' + background);
+
+    $(".line").attr("fill", color);
+    $(".box").attr("fill", background);
+    $("body").css("background-color", background);
+
+})
+
+$("#link").on('click', function(){
+
+    //get svg element.
+    var svg = $("svg")[0];
+
+    //get svg source.
+    var serializer = new XMLSerializer();
+    var source = serializer.serializeToString(svg);
+
+    //add name spaces.
+    if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+    if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+    }
+
+    //add xml declaration
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+    //convert svg source to URI data scheme.
+    var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+    //set url value to a element's href attribute.
+
+    $("#link").attr("href", url);
+
+
+});
+
+
+
+function getRandomItem() {
+    return rnd = colors[Math.floor(Math.random()*colors.length)];
+}
